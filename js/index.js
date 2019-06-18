a167c126138b4e15d48479e614a09c9c127dbdeb
TheHaunted <kevin_lopez@live.com>
2019-06-18 17:27:25 -0500

    Added image drag event listeners

--- js/index.js
+++ js/index.js
@@ -15,3 +15,18 @@ paragraphs.forEach(item => {
         item.style.border = '1px solid red';
     });
 });
+
+let images = document.querySelectorAll('img');
+images.forEach(item => {
+    item.addEventListener('dragstart', () => {
+        item.style.border = '4px solid #17A2B8';
+    });
+    item.addEventListener('drag', () => {
+        document.querySelector('body').style.background = '#5b5b5b';
+    });
+    item.addEventListener('dragend', () => {
+        item.style.border = 'none';
+        document.querySelector('body').style.background = 'white';
+    });
+});
+
