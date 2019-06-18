2aa39b38949321d2d5d834176febe72bb7ff469e
TheHaunted <kevin_lopez@live.com>
2019-06-18 17:06:54 -0500

    added mouseover, mouseout, and selectstart eventlisteners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+let destination = document.querySelectorAll('.destination');
+destination.forEach(function(item){
+    item.addEventListener('mouseover', () => {
+        item.style.background = '#17A2B8';
+    });
+    item.addEventListener('mouseout', () => {
+        item.style.background = 'white';
+    })
+});
+
+let paragraphs = document.querySelectorAll('p');
+paragraphs.forEach(item => {
+    item.addEventListener('selectstart', () => {
+        item.style.border = '1px solid red';
+    });
+});
