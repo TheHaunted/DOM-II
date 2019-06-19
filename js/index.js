2424a82e2e2e73f1836ac5f8819090ef058c39a0
TheHaunted <kevin_lopez@live.com>
2019-06-18 23:04:45 -0500

    Finished adding event listeners

--- js/index.js
+++ js/index.js
@@ -9,6 +9,13 @@ destination.forEach(function(item){
     });
 });
 
+let button = document.querySelectorAll('.btn');
+button.forEach(button => {
+    button.addEventListener('click', () => {
+        button.textContent = 'Clicked!'
+    });
+});
+
 let paragraphs = document.querySelectorAll('p');
 paragraphs.forEach(item => {
     item.addEventListener('selectstart', () => {
@@ -28,12 +35,12 @@ images.forEach(item => {
         item.style.border = '4px solid #17A2B8';
         document.querySelector('body').style.background = '#5b5b5b';
     });
-    // item.addEventListener('drag', () => {
-    //     document.querySelector('body').style.background = '#5b5b5b';
-    // });
     item.addEventListener('dragend', () => {
         item.style.border = 'none';
         document.querySelector('body').style.background = 'white';
+    });
+    item.addEventListener('dblclick', () => {
+        item.style.filter = 'invert(1)';
     });
 });
 
@@ -45,3 +52,4 @@ let page = document.querySelector('body');
 page.addEventListener('wheel', () => {
     page.style.background = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
 });
+
